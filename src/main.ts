0d9fec3b9375345bd05f1678884dc6f568947fb3
John Doe <andrewsimonmiller@gmail.com>
2023-04-26 14:56:54 +1000

    small change to main file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.PORT || 5005;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5005);
+  await app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+  });
 }
 bootstrap();
